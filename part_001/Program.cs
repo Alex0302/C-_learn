c8fbd0bd037a47c40d561dfdced01eb0de867d7e
Alex0302 <amalcev63@gmail.com>
2023-11-15 22:03:29 +0400

    первый код

---
+++ part_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string message = "Value1: ";
+Console.Write(message);
+int number1 = int.Parse(Console.ReadLine()); // считывание данных из терминала
+
+
+message = "Value2: ";
+Console.Write(message);
+int number2 = int.Parse(Console.ReadLine()); // считывание данных из терминала
+
+int result = number1 + number2;
+
+// Console.WriteLine("Result: " + result);
+
+string output = $"{number1} + {number2} = {result}";
+
+Console.WriteLine(output);
