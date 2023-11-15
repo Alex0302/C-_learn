a27c916be710c3fe7e500673e86b093ca5d6ce82
Alex0302 <amalcev63@gmail.com>
2023-11-15 22:28:59 +0400

    Часть 3

---
+++ part_003/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+Console.CursorVisible = false;
+
+int left1 = 40;
+int top1 = 1;
+Console.SetCursorPosition(left1,top1);
+Console.WriteLine("+");
+
+int left2 = 2;
+int top2 = 28;
+Console.SetCursorPosition(left2,top2);
+Console.WriteLine("+");
+
+int left3 = 80;
+int top3 = 120;
+Console.SetCursorPosition(left3,top3);
+Console.WriteLine("+");
+
+int left = (left1 + left2) / 2;
+int top = (top1 + top2) / 2;
+
+int count = 10;
+int step = 0;
+
+while (step < count) 
+{
+
+int random = Random.Shared.Next(3) + 1;
+
+if (random == 1)
+{
+    left = (left + left1) / 2;
+    top = (top + top1) / 2;
+}
+
+if (random == 2)
+{
+    left = (left + left2) / 2;
+    top = (top + top2) / 2;
+}
+
+if (random == 3)
+{
+    left = (left + left3) / 2;
+    top = (top + top3) / 2;
+}
+Console.SetCursorPosition(left,top);
+Console.WriteLine("+");
+step++;
+
+}
+
+Console.ReadLine();
