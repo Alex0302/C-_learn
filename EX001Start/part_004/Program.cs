11ce8c6dbb11869d3bc2bfbb1251cadbe8c83046
Alex0302 <amalcev63@gmail.com>
2023-11-20 19:10:50 +0400

    Массивы

--- EX001Start/part_004/Program.cs
+++ EX001Start/part_004/Program.cs
@@ -3,7 +3,20 @@
     public static void Main()
     {
         // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
+        Console.Write("Сколько элементов массива нужно: ");
+
+        int count = int.Parse(Console.ReadLine());
+        
+        int[] array = new int[count];
+
+        int index = 0;
+
+        while (index < count)
+        {
+            array[index] = Random.Shared.Next(0, 10);
+            Console.Write($"{array[index]} ");
+            index++;
+        }
 
     }
 
