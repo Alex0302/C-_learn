0b4837f3e4a8d62354c5ddf9ce1ab863fd0b331c
Alex0302 <amalcev63@gmail.com>
2023-11-26 19:15:44 +0400

    Пример полных лямбда выражений

--- EX003Linq/Program.cs
+++ EX003Linq/Program.cs
@@ -13,14 +13,11 @@ public partial class Program
         Console.WriteLine(method(1, 2));
 
 
-        int result = Calculate(20, 10, (int x, int y) =>
-    {
-        return x + y;
-    });
+        int result = Calculate(20, 10, (int x, int y) => x + y);
 
         Console.WriteLine(result);
 
-        result = Calculate(20, 10,delegate (int x, int y)
+        result = Calculate(20, 10, (int x, int y) =>
     {
         return x - y;
     });
